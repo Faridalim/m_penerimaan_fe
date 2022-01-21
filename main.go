90c74cee2c006539931d3ffaee463ca63649e4e1
Faridalim <39637832+Faridalim@users.noreply.github.com>
2022-01-21 14:25:01 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	fmt.Println("OK")
+	e := echo.New()
+	e.GET("/:namefile", pageHandler)
+	e.Logger.Fatal(e.Start("127.0.0.1:3001"))
+
+}
+
+func pageHandler(c echo.Context) error {
+	namefile := c.Param("namefile")
+	namefile = strings.TrimSuffix(namefile, "/")
+	cookie := new(http.Cookie)
+	if strings.Contains(namefile, "index.html") {
+		cookie.Name = "last_path"
+		cookie.Value = namefile
+		cookie.Expires = time.Now().Add(5 * time.Second)
+		c.SetCookie(cookie)
+		c.Response().Header().Set("Cache-Control", "max-age:2000, public")
+		return c.File("pages/k2/index.html")
+	} else {
+		c.Response().Header().Set("Cache-Control", "max-age:2000, public")
+		return c.File("pages/k2/" + namefile)
+	}
+}
+
+// func checkFile(filename string) bool {
+// 	if strings.Contains(namefile, ".js") || strings.Contains(namefile, ".js")
+// }
